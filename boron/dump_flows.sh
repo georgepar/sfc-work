efad0470cf8a7df7e936e4b6ebff8333ecf0bcd7
George Paraskevopoulos <georgepar.91@gmail.com>
2016-08-18 12:45:02 +0300

    add boron testcase files

---
+++ boron/dump_flows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ovs-ofctl -O Openflow13 dump-flows br-int table=11 | grep "nsp="
+ovs-ofctl -O Openflow13 dump-flows br-int table=11 | grep NXM_NX_NSH_C1
+ovs-ofctl -O Openflow13 show br-int | grep vxgpe
