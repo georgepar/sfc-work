fa343cf658bc3b8ed6da4d94e066559aa3df801d
George Paraskevopoulos <georgepar.91@gmail.com>
2016-08-19 16:31:08 +0300

    Update dump_flows.sh

--- boron/dump_flows.sh
+++ boron/dump_flows.sh
@@ -1,4 +1,1 @@
-#ovs-ofctl -O Openflow13 dump-flows br-int table=11 | grep "nsp="
-#ovs-ofctl -O Openflow13 dump-flows br-int table=11 | grep NXM_NX_NSH_C1
-#ovs-ofctl -O Openflow13 show br-int | grep vxgpe
-ovs-ofctl -O Openflow13 dump-flows br-int table=11
+ovs-ofctl -O Openflow13 dump-flows br-int | cut -d',' -f3-
